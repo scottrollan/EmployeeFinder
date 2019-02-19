888f28958b20b9387fe8bcfff977be3bddac2fdb
Barry Scott Rollan <b.scott.rollan@gmail.com>
2019-02-19 14:42:17 -0500

    git@github.com:scottrollan/EmployeeFinder.git

--- app/data/employees.js
+++ app/data/employees.js
@@ -1,102 +1,101 @@
 // This array will hold all the employees and their scores
 
-var employeeList = [
-    {
-        name:   "Ahmed",
-        photo:  "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [
-            5,
-            1,
+var employees = [
+        {
+            name: "Daenerys Targaryen",
+            photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/4/4f/Daenerys_Season_8.jpg/revision/latest?cb=20190206213056",
+            scores: [
+                1,
+                1,
+                2,
+                1,
+                1,
+                5,
+                3,
+                1,
+                2,
+                3
+            ]
+        },
+        {
+            name: "The Green Arrow a.k.a. Oliver Queen",
+            photo: "https://pbs.twimg.com/profile_images/564674987152662529/I4DSTYl8_400x400.jpeg",
+            scores: [
             4,
             4,
-            5,
-            1,
             2,
-            5,
-            4,
-            1
-          ]
-    },
-    {
-        name: "Daenerys Targaryen",
-        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/4/4f/Daenerys_Season_8.jpg/revision/latest?cb=20190206213056",
-        scores: [
-            1,
-            5,
             2,
-            1,
-            1,
-            5,
-            3,
-            1,
-            5,
-            3
-        ]
-    },
-    {
-        name: "The Green Arrow a.k.a. Oliver Queen",
-        photo: "https://static.webshopapp.com/shops/002090/files/174853571/arrow-real-master-series-action-figure-1-8-green-a.jpg",
-        scores: [
-           4,
-           4,
-           2,
-           4,
-           2,
-           3,
-           2,
-           1,
-           5,
-           4
-        ]
-    },
-    {
-        name: "Dexter Morgan",
-        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Dexter_Morgan.jpg/200px-Dexter_Morgan.jpg",
-        scores: [
-         4,
-         5,
-         1,
-         5,
-         3,
-         1,
-         1,
-         3,
-         5,
-         5
-        ]
-    },
-    {
-        name: "Captain James T. Kirk of the U.S.S. Enterprise",
-        photo: "https://cdn-images-1.medium.com/max/1200/1*4tk-fUy-dqYs4R7TnrJZ-w.jpeg",
-        scores: [
-            2,
-            3,
-            5,
-            1,
             2,
             4,
-            3,
             2,
-            3,
+            1,
+            1,
             4
-        ]
-    },
-    {
-        name: "Kimmy Schmidt, the unbreakable",
-        photo: "https://pixel.nymag.com/imgs/daily/vulture/2018/05/29/29-unbreakable-kimmy-schmidt.w700.h700.jpg",
-        scores: [
-            1,
-            5,
-            5,
-            5,
-            3,
-            5,
-            5,
-            3,
-            5,
-            5
-        ]
-    }
-]
+            ]
+        },
+        {
+            name: "Dexter Morgan",
+            photo: "https://static.giantbomb.com/uploads/scale_small/3/39164/1183089-untitled_1.png",
+            scores: [
+                2,
+                5,
+                1,
+                5,
+                3,
+                1,
+                1,
+                3,
+                1,
+                5
+            ]
+        },
+        {
+            name: "Captain James T. Kirk of the U.S.S. Enterprise",
+            photo: "https://cdn-images-1.medium.com/max/1200/1*4tk-fUy-dqYs4R7TnrJZ-w.jpeg",
+            scores: [
+                2,
+                3,
+                5,
+                1,
+                2,
+                4,
+                3,
+                2,
+                3,
+                4
+            ]
+        },{
+            name: "Raj Koothrappali",
+            photo: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201607/kunalstory_647_072816080151.jpg",
+            scores: [
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4
+            ]
+        },
+        {
+            name: "Kimmy Schmidt, the unbreakable",
+            photo: "https://pixel.nymag.com/imgs/daily/vulture/2018/05/29/29-unbreakable-kimmy-schmidt.w700.h700.jpg",
+            scores: [
+                1,
+                5,
+                5,
+                5,
+                3,
+                5,
+                5,
+                3,
+                4,
+                5
+            ]
+        }
+    ];
 
-module.exports = employeeList;
+module.exports =  employees;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,50 @@
-let employeeList = require('../data/employees.js');
+let employees = require('../data/employees.js');
 
-//GET the employeeList
+//GET the employees
 module.exports = function (app) {
-    app.get('/api/employeeList', function (req, res) {
-        res.json(employeeList);
-      });
+  app.get('/api/employees', function (req, res) {
+      res.json(employees);
+  });
+  
+  // process after user submit
+  app.post('/api/employees', function(req, res) {
+      const bestMatch = {
+      name: '',
+      photo: '',
+      employeeDifference: 41
+    }
+  
+    const userData = req.body;
+    const userScores = userData.scores;
+    
+    let totalDifference;
 
-    app.post('/api/employeeList', function(req, res) {
-      employeeList.push(req.body);
+    //loop through the already set-up employees list
+    for(let i=0; i<employees.length; i++) {
+      const currentEmployee = employees[i];
+      totalDifference = 0;
 
-    res.end();
-    });
-}    
+      //loop through the scores array inside each employee, as provided from loop above and
+      //compare user input scores (currentUserScore) to currentEmployee.scores and
+      //caluculate numerical difference in absolute value
+      for(let j = 0; j<currentEmployee.scores.length; j++) {
+        const currentEmployeeScore = currentEmployee.scores[j];
+        const currentUserScore = userScores[j];
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentEmployeeScore));
+
+      }
+    
+      //replace bestMatch (and, hence, .name and .photo values) with any  
+      //employee whose totalDifference is less than the ones before
+      if (totalDifference <= bestMatch.employeeDifference) {
+        bestMatch.name = currentEmployee.name;
+        bestMatch.photo = currentEmployee.photo;
+        bestMatch.employeeDifference = totalDifference;
+      }
+    }
+    console.log(bestMatch);
+    employees.push(userData);
+
+  });
+};
+   
